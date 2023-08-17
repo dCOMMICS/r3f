154289b89e22c75bf8f78f600707d6c987a69333
dCOMMICS <sanwurld@gmail.com>
2023-08-17 13:40:03 +0300

    red box created with the given parameters and the given parameters are expected to be present in the output file.

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ console.log(THREE)
 
 // threejs will generate a scene  //console    
 const scene = new THREE.Scene();
+
+// red box //
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
