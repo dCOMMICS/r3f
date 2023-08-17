7688db3e85d3e2a360fd2e5cd706d73ca91d9ee3
dCOMMICS <sanwurld@gmail.com>
2023-08-17 15:43:26 +0300

    camera work

--- script.js
+++ script.js
@@ -9,5 +9,12 @@ const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
+const Mesh = new THREE.Mesh(geometry, material)
+ // const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+// camera //
+const camera = new THREE.PerspectiveCamera();
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.add(camera);
+camera.position.z = 5;
