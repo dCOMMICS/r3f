b1d047ff64a3bb2f9354d6f51cd7eaa050a9dfc8
dCOMMICS <sanwurld@gmail.com>
2023-08-17 15:16:23 +0300

    good condition

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ const scene = new THREE.Scene();
 // red box //
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000});
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
