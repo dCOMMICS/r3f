9aca1f8ee0d8e272edafca95dc4919fdbb3852f2
dCOMMICS <sanwurld@gmail.com>
2023-08-19 15:56:20 +0300

    rendering

--- script.js
+++ script.js
@@ -18,4 +18,12 @@ scene.add(cube);
 const camera = new THREE.PerspectiveCamera();
 // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 scene.add(camera);
-camera.position.z = 5;
+camera.position.z = 5 ;
+
+// renderer //
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// light //
