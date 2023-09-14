999fd2dc142a275fe56f3da6f659fee5c9bbd596
dCOMMICS <sanwurld@gmail.com>
2023-09-15 00:42:34 +0300

    filtered (arrays in javascript)

--- s.js
+++ s.js
@@ -6,3 +6,20 @@ function consoleItem(item,index,arr) {
     console.log(arr)
     
 }
+
+// array of items//
+
+const items = [
+    {name:'food',  price: 110},
+    {name:'transport',  price: 70},
+    {name:'airtime',  price: 50},
+    {name:'laptop',  price: 23000},
+    {name:'entertainment',  price: 310},
+    {name:'water',  price: 150},
+];
+
+const filteredItems = items.filter((item) => {
+return item.price >= 70         // if price is greater  than  or equal to   70 . 
+})
+
+console.log(filteredItems)
